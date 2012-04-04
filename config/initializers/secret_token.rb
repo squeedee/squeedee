62c4c428754fd91c87d960af0e7bb66f02d9632c
squeedee <squeedee@gmail.com>
2012-04-04 19:15:05 +1200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squeedee::Application.config.secret_token = '88f4fa1ecdbdafab6aea31025baac6331cebf270e4f122507b7465dbef725e55a1d925d6fc0e1ab4a2798c783468e0ba8da55ff5bdf50b3f69ad4fbdcc9aedb8'
